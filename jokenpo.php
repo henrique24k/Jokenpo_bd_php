7fb5ddfb6b7817fdd454c4345b45066290a8726c
Henrique Vieira <henriquevieira431@gmail.com>
2022-09-29 22:03:49 -0300

    adição da tb ranking

--- jokenpo.php
+++ jokenpo.php
@@ -8,6 +8,10 @@
 </head>
 <body>
 
+<script src="script.js">
+
+</script>
+
     <h4>Digite 0 para pedra 1 para papel e 2 para tesoura </h4>
 
     <form method="get">
@@ -17,8 +21,6 @@
     </form>
 
 <?php
-
-    echo "Seja muito bem-vindo ao Jokenpo </br> </br>";
     $nomeJogador = $_GET['nome'];
     $opcoesDeJogada = ["PEDRA", "PAPEL", "TESOURA"];
     $sorteio = rand(0, 2);
@@ -64,6 +66,7 @@
                 break;
         }
     }
+
 ?>
 
 <?php
@@ -74,12 +77,33 @@ $pass = "root";
 $base = "jokenpo";
 $con = mysqli_connect($host, $user, $pass, $base);
 
-$res = mysqli_query($con,"insert into ranking (nome, opcao) values ('$nomeJogador', '$escolhaJogador')");
+$res = mysqli_query($con,"insert into ranking (nome, opcao, sorteio) values ('$nomeJogador', '$escolhaJogador', '$sorteio')");
 echo"Cadastro realizado com sucesso! </br>";
 
 mysqli_close($con);
 
 ?>
+<h1>Ranking</h1>
+<?php
+        $host = "localhost:3306";
+        $user = "root";
+        $pass = "root";
+        $base = "jokenpo";
+        $con = mysqli_connect($host, $user, $pass, $base);
+        $res = mysqli_query ($con, "select * from ranking");
+
+        echo "<table border=3px> <tr> <td> Colocação </td> <td> Nome do Usuário </td> <td> Opção do Usuário </td> <td> Opção da máquina </td> </tr>"; 
+
+while ($resultados = mysqli_fetch_array($res)){
+    echo"</td> <td>" . $resultados['colocao'] . "</td> <td>" . $resultados['nome'] . "</td> <td>" . $resultados['opcao'] . "</td> <td>" . $resultados['sorteio'] . "</td> </tr>";
+}
+
+echo "</table> </br> </br>";
+
+mysqli_close($con);
+
+?>
+
 </body>
 
 </html>
